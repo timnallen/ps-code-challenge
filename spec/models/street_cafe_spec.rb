c934a320ac9d2fcd881155dce4536aeabc9fac32
Tim Allen <tnelsallen3@gmail.com>
2019-10-03 15:52:41 -0600

    Adds SQL view with associated class method and test

--- spec/models/street_cafe_spec.rb
+++ spec/models/street_cafe_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe StreetCafe, type: :model do
   describe 'class methods' do
     it '::data_by_post_code' do
-      StreetCafe.create('café/restaurant_name': 'All Bar One', street_address: '27 East Parade', post_code: 'LS1 5BN', number_of_chairs: 20)
+      StreetCafe.create('café/restaurant_name': 'All Bar Two', street_address: '27 East Parade', post_code: 'LS1 5BN', number_of_chairs: 20)
       StreetCafe.create('café/restaurant_name': 'All Bar One', street_address: 'Unit D Electric Press, 4 Millenium Square', post_code: 'LS2 3AD', number_of_chairs: 140)
       StreetCafe.create('café/restaurant_name': 'Peachy Keens', street_address: 'Electric Press Building', post_code: 'LS2 3AD', number_of_chairs: 96)
 
-      expect(StreetCafe.data_by_post_code[0].post_code).to eq('LS1 5BN')
-      expect(StreetCafe.data_by_post_code[0].total_places).to eq(1)
-      expect(StreetCafe.data_by_post_code[0].total_chairs).to eq(20)
-      expect(StreetCafe.data_by_post_code[0].chairs_pct).to eq(7.0)
-      expect(StreetCafe.data_by_post_code[0].place_with_max_chairs).to eq('All Bar One')
-      expect(StreetCafe.data_by_post_code[0].max_chairs).to eq(20)
+      expect(StreetCafe.data_by_post_code[0][0]).to eq('LS1 5BN')
+      expect(StreetCafe.data_by_post_code[0][1]).to eq(1)
+      expect(StreetCafe.data_by_post_code[0][2]).to eq(20)
+      expect(StreetCafe.data_by_post_code[0][3]).to eq(20)
+      expect(StreetCafe.data_by_post_code[0][4]).to eq("7.81")
+      expect(StreetCafe.data_by_post_code[0][5]).to eq('All Bar Two')
 
-      expect(StreetCafe.data_by_post_code[1].post_code).to eq('LS2 3AD')
-      expect(StreetCafe.data_by_post_code[1].total_places).to eq(2)
-      expect(StreetCafe.data_by_post_code[1].total_chairs).to eq(236)
-      expect(StreetCafe.data_by_post_code[1].chairs_pct).to eq(92.0)
-      expect(StreetCafe.data_by_post_code[1].place_with_max_chairs).to eq('All Bar One')
-      expect(StreetCafe.data_by_post_code[1].max_chairs).to eq(140)
+      expect(StreetCafe.data_by_post_code[1][0]).to eq('LS2 3AD')
+      expect(StreetCafe.data_by_post_code[1][1]).to eq(2)
+      expect(StreetCafe.data_by_post_code[1][2]).to eq(236)
+      expect(StreetCafe.data_by_post_code[1][3]).to eq(140)
+      expect(StreetCafe.data_by_post_code[1][4]).to eq("92.19")
+      expect(StreetCafe.data_by_post_code[1][5]).to eq('All Bar One')
     end
   end
 end
